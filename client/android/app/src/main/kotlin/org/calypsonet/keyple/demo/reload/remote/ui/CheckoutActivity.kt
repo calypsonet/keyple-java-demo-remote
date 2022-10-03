ed096780b97e1be0f77b646d38894ddd03b3518f
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2022-10-03 09:45:11 +0200

    fix: type conversion

--- client/android/app/src/main/kotlin/org/calypsonet/keyple/demo/reload/remote/ui/CheckoutActivity.kt
+++ client/android/app/src/main/kotlin/org/calypsonet/keyple/demo/reload/remote/ui/CheckoutActivity.kt
@@ -31,9 +31,9 @@ class CheckoutActivity : AbstractDemoActivity() {
     val selectedTicketPriorityCode =
         PriorityCode.findEnumByKey(
             intent
-                .getByteExtra(
+                .getIntExtra(
                     SelectTicketsActivity.SELECTED_TICKET_PRIORITY_CODE,
-                    PriorityCode.MULTI_TRIP.key.toByte())
+                    PriorityCode.MULTI_TRIP.key)
                 .toInt())
     val ticketNumberCount: Int = intent.getIntExtra(SelectTicketsActivity.TICKETS_NUMBER, 0)
 
