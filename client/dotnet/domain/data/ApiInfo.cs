d9466b961ac60c8a6647e5cccec2706e01ef5337
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2023-12-06 16:15:59 +0100

    fix: add missing file

---
+++ client/dotnet/domain/data/ApiInfo.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2023 Calypso Networks Association https://calypsonet.org/
+//
+// See the NOTICE file(s) distributed with this work for additional information
+// regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the terms of the
+// Eclipse Public License 2.0 which is available at http://www.eclipse.org/legal/epl-2.0
+//
+// SPDX-License-Identifier: EPL-2.0
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.domain.data
+{
+    internal class ApiInfo
+    {
+        public const int API_LEVEL = 2;
+        public const int CORE_API_LEVEL = 2;
+    }
+}
